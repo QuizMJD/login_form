651cb182c25d83cd05e35006e6c196f502789266
QuizMJD <darkscopio2@mail.com>
2023-08-19 22:48:18 +0700

    fixed bugs done

--- src/main/java/com/example/login_form/LoginServlet.java
+++ src/main/java/com/example/login_form/LoginServlet.java
@@ -5,8 +5,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.example.login_form.db.JdbcConnection;
+
 import java.io.IOException;
 import java.sql.*;
+import java.util.Scanner;
 
 
 @WebServlet("/login")
@@ -17,7 +21,12 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
 
         // Thực hiện kiểm tra đăng nhập trong cơ sở dữ liệu
-        boolean isValidUser = validateUser(username, password);
+        boolean isValidUser = false;
+        try {
+            isValidUser = validateUser(username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         if (isValidUser) {
             // Đăng nhập thành công, chuyển hướng đến trang chính
@@ -27,48 +36,24 @@ public class LoginServlet extends HttpServlet {
             request.setAttribute("errorMessage", "Invalid username or password");
             request.getRequestDispatcher("login.jsp").forward(request, response);
         }
+
+
+
+
+
     }
 
-    private boolean validateUser(String username, String password) {
+    private boolean validateUser (String username, String password) throws SQLException {
         // Thực hiện truy vấn đến cơ sở dữ liệu để kiểm tra thông tin đăng nhập
         // Trả về true nếu thông tin đúng, false nếu thông tin sai
         // Ví dụ sử dụng JDBC để truy vấn cơ sở dữ liệu
         // Đây chỉ là ví dụ, bạn cần thay thế bằng mã xử lý thực tế
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
 
-        try {
-            // Tạo kết nối đến cơ sở dữ liệu
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db", "root", "123456");
-
-            // Tạo truy vấn kiểm tra thông tin đăng nhập
-            String query = "SELECT COUNT(*) FROM users WHERE username = ? AND password = ?";
-            stmt = conn.prepareStatement(query);
-            stmt.setString(1, username);
-            stmt.setString(2, password);
-
-            // Thực thi truy vấn
-            rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                int count = rs.getInt(1);
-                return count > 0;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Đóng các kết nối và tài nguyên
-            try {
-                if (rs != null) rs.close();
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return false;
+        JdbcConnection jdbcConnection = new JdbcConnection();
+        return jdbcConnection.login(username, password);
     }
+
+
+
 }
+
---
+++ src/main/java/com/example/login_form/db/JdbcConnection.java
@@ -0,0 +1,82 @@
+package com.example.login_form.db;
+
+import java.sql.*;
+
+public class JdbcConnection {
+     // Method to create the connection and set up database resources
+    public static Connection createConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db", "root", "123456");
+    }
+
+    // Method to close the connection and other resources
+    private static void closeResources(Connection conn, Statement stm, ResultSet rs) {
+        try {
+            if (rs != null) rs.close();
+            if (stm != null) stm.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to insert a new brand into the database
+    private static int insertBrand(Connection conn, String name) throws SQLException {
+        String insertQuery = "INSERT INTO users (name) VALUES (?)";
+        PreparedStatement insertStatement = conn.prepareStatement(insertQuery);
+        insertStatement.setString(1, name);
+        return insertStatement.executeUpdate();
+    }
+
+    // Method to update the name of a brand based on its ID
+    private static int updateBrandName(Connection conn, int id, String newName) throws SQLException {
+        String updateQuery = "UPDATE users SET name = ? WHERE id = ?";
+        PreparedStatement updateStatement = conn.prepareStatement(updateQuery);
+        updateStatement.setString(1, newName);
+        updateStatement.setInt(2, id);
+        return updateStatement.executeUpdate();
+    }
+
+    // Method to delete a brand based on its ID
+    private static int deleteBrandById(Connection conn, int id) throws SQLException {
+        String deleteQuery = "DELETE FROM users WHERE id = ?";
+        PreparedStatement deleteStatement = conn.prepareStatement(deleteQuery);
+        deleteStatement.setInt(1, id);
+        return deleteStatement.executeUpdate();
+    }
+
+    // Method to search for a brand by its name
+    private static void searchBrandByName(Connection conn, String searchKeyword) throws SQLException {
+        String searchQuery = "SELECT * FROM users WHERE name LIKE ?";
+        PreparedStatement searchStatement = conn.prepareStatement(searchQuery);
+        searchStatement.setString(1, "%" + searchKeyword + "%");
+        ResultSet rs = searchStatement.executeQuery();
+
+        // Display search results
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            System.out.println("ID: " + id + ", Name: " + name);
+        }
+    }
+    public static boolean login(String username, String password) throws SQLException {
+        Connection conn = null;
+        boolean status = false;
+        try {
+            conn = createConnection();
+            String searchQuery = "SELECT * FROM users WHERE username = ? AND password = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(searchQuery);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            ResultSet rs = preparedStatement.executeQuery();
+            status = rs.next();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources(conn, null, null);
+        }
+        return status;
+
+    }
+}
+//
